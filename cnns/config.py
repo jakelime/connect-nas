0779a922ece5fdc7e7427dc2241b48c91b192503
jakelime <jiahao89sg@gmail.com>
2023-10-24 12:56:37 +0800

    fixed config.py

--- cnns/config.py
+++ cnns/config.py
@@ -1,6 +1,11 @@
+import os
+import dotenv
 
-paths_db = {
-    # Key: smb address
-    # Value: mounted local address
-    # e.g. "smb://10.10.1.2/photos": "/Volumes/photos"
-}
+dotenv.load_dotenv()
+
+paths_db = {}
+# Key: smb address
+# Value: mounted local address
+# e.g. "smb://10.10.1.2/photos": "/Volumes/photos"
+# "smb://192.168.50.243/home": "/Volumes/home"
+paths_db[os.getenv("NAS_ADDR01_SMB")] = os.getenv("NAS_ADDR01_LOCAL")
