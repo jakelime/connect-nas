92394b13f10f677a3d84e3171917001463222a1c
Jake Lim <jiahao89sg@gmail.com>
2024-05-27 15:38:22 +0800

    add pip commands.sh

---
+++ pip_commands.sh
@@ -0,0 +1,8 @@
+## Run these commands first, manually
+# python -m venv venv
+# source venv/bin/activate
+
+## Once your terminal is hooked up to the
+## new python venv, then run this scrip to trigger
+## these pip installs
+pip install pyinstaller
